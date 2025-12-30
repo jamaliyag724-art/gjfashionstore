696fb656eb62da571989035c86f1e8e91fee9406
jamaliyag724-art <jamaliyag724@gmail.com>
2025-12-30 16:53:26 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqwM1rr_5BCCZLdANSCJwhN0gUykEeC-A",
+  apiKey: "AIzaSyAqwM1rr_5BCCZLDANscJwhN0gUykeC-A",
   authDomain: "gj-fashion-store.firebaseapp.com",
   projectId: "gj-fashion-store",
   storageBucket: "gj-fashion-store.firebasestorage.app",
   messagingSenderId: "497143355906",
   appId: "1:497143355906:web:bea21416e412be0241e51b",
+  measurementId: "G-48F8R1NLM"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export default app;
