14c79fcec0970659f791f3afa17544b48e0dd18e
jamaliyag724-art <jamaliyag724@gmail.com>
2025-12-30 18:21:12 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyAqwM1rr_5BCCZLdANSCJwhN0gUykEeC-A",
   authDomain: "gj-fashion-store.firebaseapp.com",
   projectId: "gj-fashion-store",
-  storageBucket: "gj-fashion-store.firebasestorage.app",
+  storageBucket: "gj-fashion-store.appspot.com",
   messagingSenderId: "497143355906",
-  appId: "1:497143355906:web:bea21416e412be0241e51b",
+  appId: "1:497143355906:web:bea21416e412be0241e51b"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const apiKey = firebaseConfig.apiKey;
