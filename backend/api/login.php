9d7a1f102aea7631f5d82f73f1f2c340af077b5c
jamaliyag724-art <jamaliyag724@gmail.com>
2026-01-07 19:30:21 +0530

    Add CORS headers to login API

--- backend/api/login.php
+++ backend/api/login.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:8080");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 require_once "../config.php";
 
 $data = json_decode(file_get_contents("php://input"), true);
@@ -30,3 +40,4 @@ echo json_encode([
     ]
 ]);
 ?>
+
