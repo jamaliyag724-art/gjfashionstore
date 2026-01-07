adf2398aab337312d7208d3cf2da3e1405aacfa3
jamaliyag724-art <jamaliyag724@gmail.com>
2026-01-07 19:30:46 +0530

    Add CORS headers and handle preflight requests

--- backend/api/register.php
+++ backend/api/register.php
@@ -1,4 +1,15 @@
 <?php
+
+header("Access-Control-Allow-Origin: http://localhost:8080");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 require_once "../config.php";
 
 $data = json_decode(file_get_contents("php://input"), true);
@@ -44,3 +55,4 @@ catch (PDOException $e) {
     }
 }
 ?>
+
